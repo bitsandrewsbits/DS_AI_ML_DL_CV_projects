66a6ddc1c4ac73845dc32d71482947331a5bef47
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-01-04 23:57:54 +0200

    import some libs. start create init method for K-means.

--- K-Means_Clustering/first_K-Means_clustering.py
+++ K-Means_Clustering/first_K-Means_clustering.py
@@ -1,5 +1,11 @@
 # A first realization of K-Means Clustering classification algorithm
 # for any datasets
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 class K_Means_Clustering:
-    pass
+    def __init__(self, dataset_name: str):
+        self.dataset = pf.read_csv(dataset_name)
+        
