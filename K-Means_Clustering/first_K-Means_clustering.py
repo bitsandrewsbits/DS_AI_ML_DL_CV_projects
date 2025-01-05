59546151ab6d2294cbf2586bd593b5a91271a020
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-01-05 22:33:02 +0200

    create partially prepare data method.

--- K-Means_Clustering/first_K-Means_clustering.py
+++ K-Means_Clustering/first_K-Means_clustering.py
@@ -8,4 +8,15 @@ import matplotlib.pyplot as plt
 class K_Means_Clustering:
     def __init__(self, dataset_name: str):
         self.dataset = pf.read_csv(dataset_name)
-        
+        self.clusters_amount = 0
+
+    def main(self):
+        self.prepare_data()
+
+    def prepare_data(self):
+        print('[INFO] Cleaning data from missing values...')
+        self.dataset.dropna(inplace = True)
+        # TODO: finish method
+
+if __name__ == "__main__":
+    k_means_classifier = K_Means_Clustering("your_dataset.csv")
