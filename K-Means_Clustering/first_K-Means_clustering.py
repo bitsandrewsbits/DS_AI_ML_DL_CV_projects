0712a13e879797cc464ce88a339c6d4264797cfd
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-01-06 22:11:01 +0200

    create data scaler method for better K-means model accuracy.

--- K-Means_Clustering/first_K-Means_clustering.py
+++ K-Means_Clustering/first_K-Means_clustering.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 class K_Means_Clustering:
     def __init__(self, dataset_name: str):
-        self.dataset = pf.read_csv(dataset_name)
+        self.dataset = pd.read_csv(dataset_name)
         self.clusters_amount = 0
 
     def main(self):
@@ -16,7 +16,18 @@ class K_Means_Clustering:
     def prepare_data(self):
         print('[INFO] Cleaning data from missing values...')
         self.dataset.dropna(inplace = True)
-        # TODO: finish method
+        self.scale_numeric_dataset_columns()
+
+    def scale_numeric_dataset_columns(self):
+        scaler = StandardScaler()
+        # check if column - is number type column and transform through
+        # standard scaler
+        for column in self.dataset.columns:
+            if self.dataset[column].dtypes != object:
+                reshaped_df_column = self.dataset[column].values.reshape(-1, 1)
+                self.dataset[column] = scaler.fit_transform(reshaped_df_column)
+        print(self.dataset)
 
 if __name__ == "__main__":
     k_means_classifier = K_Means_Clustering("your_dataset.csv")
+    k_means_classifier.main()
