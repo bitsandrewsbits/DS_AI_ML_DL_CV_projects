0b08ffc8ee064d2b4e2860d81ee12e31c9fd9c56
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-01-07 23:02:56 +0200

    create methods for defining n_clusters parameter for first training K-means model.

--- K-Means_Clustering/first_K-Means_clustering.py
+++ K-Means_Clustering/first_K-Means_clustering.py
@@ -1,6 +1,7 @@
 # A first realization of K-Means Clustering classification algorithm
 # for any datasets
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
@@ -8,7 +9,7 @@ import matplotlib.pyplot as plt
 class K_Means_Clustering:
     def __init__(self, dataset_name: str):
         self.dataset = pd.read_csv(dataset_name)
-        self.clusters_amount = 0
+        self.clusters_amount = self.get_n_clusters_for_dataset()
 
     def main(self):
         self.prepare_data()
@@ -20,13 +21,32 @@ class K_Means_Clustering:
 
     def scale_numeric_dataset_columns(self):
         scaler = StandardScaler()
-        # check if column - is number type column and transform through
-        # standard scaler
         for column in self.dataset.columns:
             if self.dataset[column].dtypes != object:
                 reshaped_df_column = self.dataset[column].values.reshape(-1, 1)
                 self.dataset[column] = scaler.fit_transform(reshaped_df_column)
         print(self.dataset)
+
+    def get_n_clusters_for_dataset(self):
+        print('[INFO] Defining n_clusters parameter for K-means model...')
+        sorted_unique_values_by_columns = sorted(self.get_unique_values_amount_by_columns())
+        median_number = np.median(sorted_unique_values_by_columns)
+        n_clusters_parameter = 0
+        for column_unique_values_amount in sorted_unique_values_by_columns:
+            if median_number > column_unique_values_amount * 2:
+                n_clusters_parameter = column_unique_values_amount
+            else:
+                break
+        print('n_clusters =', n_clusters_parameter)
+        return n_clusters_parameter
+
+    def get_unique_values_amount_by_columns(self):
+        print('[INFO] Defining amount of unique values by columns...')
+        unique_values_amount_by_columns = []
+        for column in self.dataset.columns:
+            column_unique_values_amount = len(self.dataset[column].unique())
+            unique_values_amount_by_columns.append(column_unique_values_amount)
+        return unique_values_amount_by_columns
 
 if __name__ == "__main__":
     k_means_classifier = K_Means_Clustering("your_dataset.csv")
