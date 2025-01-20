74d587a4fc0fa3218279bf688a0f7677d5cd6c77
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-01-20 15:30:49 +0200

    create method for removing spec symbols from Review column, using re lib.

--- Logistic_Regression/Text_Classification_for_Sentiment_Analysis/text_classification_for_sentiment_analysis.py
+++ Logistic_Regression/Text_Classification_for_Sentiment_Analysis/text_classification_for_sentiment_analysis.py
@@ -1,5 +1,6 @@
 # Text Classification for Sentiment Analysis
 
+import re
 import pandas as pd
 
 dataset = pd.read_csv('customer_reviews_sentiment.csv')
@@ -28,13 +29,19 @@ def dataset_has_missing_values():
 
 def text_preprocessing():
     convert_review_column_to_lowercase()
+    remove_any_spec_chars_in_review_column()
 
 def convert_review_column_to_lowercase():
     text_column = dataset['Review']
     dataset['Review'] = text_column.str.lower()
 
-def remove_any_spec_chars_in_text():
-    pass
+def remove_any_spec_chars_in_review_column():
+    spec_chars = [',', '.', '!']
+    for i in range(len(dataset['Review'])):
+        for spec_char in spec_chars:
+            spec_char_regex = rf"\{spec_char}"
+            target_review_text = dataset['Review'][i]
+            dataset.loc[i, 'Review'] = re.sub(spec_char_regex, '', target_review_text)
 
 if __name__ == '__main__':
     sentiment_analysis()
