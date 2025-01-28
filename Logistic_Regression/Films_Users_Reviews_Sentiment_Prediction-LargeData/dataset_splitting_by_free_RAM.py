7013603d66a0a6652475b006e35e69381ec9e80a
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-01-28 21:24:04 +0200

    create new dir project - films reviews sentiment prediction using large dataset. create file for program split large dataset into minidatasets by optimal free RAM.

---
+++ Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/dataset_splitting_by_free_RAM.py
@@ -0,0 +1,7 @@
+# this is program for analyzing your free RAM and
+# compute optimal size of minidatasets for reading, changing operations.
+# Minidatasets will be writting into disk into dir where original large dataset exists.
+
+class Dataset_Splitting_by_free_RAM:
+    def __init__(self, dataset_name: str):
+        pass
