28695b53c223c6ca2ec66dd33554738c9d56381b
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-02-02 17:03:42 +0200

    change script at all. only add method for createing first 10 minidatasets with 1M rows.

--- Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/dataset_splitting_by_free_RAM.py
+++ Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/dataset_splitting_by_free_RAM.py
@@ -4,72 +4,93 @@
 
 import psutil
 import os
-import shutil
+import gc
 import pandas as pd
 
 class Dataset_Splitting_by_free_RAM:
     def __init__(self, csv_dataset_path: str):
         self.csv_dataset_path = csv_dataset_path
         self.free_memory_for_minidataset = self.get_optimal_free_RAM()
-        self.memory_of_one_row_in_bytes = self.get_approximately_row_per_memory_in_bytes()
-        self.dataset_rows_amount = self.get_approximately_rows_amount_in_dataset()
-        self.rows_amount_for_minidataset = self.get_approximately_rows_amount_for_minidataset()
+        self.rows_amount_for_minidataset = 10 ** 6
+
+    def create_first_10_minidatasets_with_million_rows(self):
+        for i in range(1, 11):
+            try:
+                print(f'[INFO] Making Minidataset #{i}...', end = ' ')
+                minidataset = pd.read_csv(self.csv_dataset_path,
+                    nrows = self.rows_amount_for_minidataset,
+                    skiprows = self.rows_amount_for_minidataset * i)
+                self.save_minidataset_to_csv(minidataset, i)
+                del minidataset
+                gc.collect()
+                print('DONE')
+            except:
+                print('[INFO] End of CSV dataset. exitting...')
+                break
 
     def get_optimal_free_RAM(self):
         virt_memory = psutil.virtual_memory()
-        free_RAM_in_GB = virt_memory.available / (1024 * 1024 * 1024)
-        optimal_free_RAM_for_minidataset = int(free_RAM_in_GB) - 1
-        if optimal_free_RAM_for_minidataset > 0:
-            print('Optimal RAM for minidataset')
+        total_RAM_in_GB = int(virt_memory.total / (2 ** 30))
+        free_RAM_in_GB = int(virt_memory.available / (2 ** 30))
+        print(f'Free RAM = {free_RAM_in_GB}GB')
+        if free_RAM_in_GB >= 1 and total_RAM_in_GB > 4:
+            optimal_free_RAM_for_minidataset = free_RAM_in_GB / total_RAM_in_GB
+            print('Optimal RAM for minidataset:')
             print(f'for reading, changing operations = {optimal_free_RAM_for_minidataset}GB')
             return optimal_free_RAM_for_minidataset
         else:
             print('Your free RAM very low. If you want to continue, release memory.')
             return False
 
-    def main(self):
-        if not self.dataset_copy_exist():
-            self.make_copy_of_original_dataset_file()
+    def split_dataset_into_minidatasets(self):
+        minidataset_number = 1
+        iteration_counter = 0
+        while True:
+            try:
+                print(f'[INFO] Making Minidataset #{minidataset_number}...', end = ' ')
+                minidataset = pd.read_csv(self.csv_dataset_path,
+                    nrows = self.rows_amount_for_minidataset,
+                    skiprows = self.rows_amount_for_minidataset * iteration_counter)
+                self.save_minidataset_to_csv(minidataset, minidataset_number)
+                minidataset_number += 1
+                iteration_counter += 1
+                del minidataset
+                gc.collect()
+                print('DONE')
+            except:
+                print('[INFO] End of CSV dataset. exitting...')
+                break
 
-    def dataset_copy_exist(self):
-        data_dir_filenames = os.listdir('data')
-        return 'copy_of_dataset_user_reviews.csv' in data_dir_filenames
+    def save_minidataset_to_csv(self, minidataset_df, minidataset_number: int):
+        minidataset_df.to_csv(f'data/minidataset_{minidataset_number}', index = False)
 
-    def make_copy_of_original_dataset_file(self):
-        print('[INFO] Make a dataset copy for further splitting another one...')
-        path_for_copy_of_dataset = 'data/copy_of_dataset_user_reviews.csv'
-        os.system(f"rsync {self.csv_dataset_path} {path_for_copy_of_dataset}")
+    def define_rows_amount_for_minidataset_optimal_RAM(self):
+        free_RAM_in_bytes = self.free_memory_for_minidataset * (2 ** 30)
+        print('RAM for minidataset in bytes:', free_RAM_in_bytes)
+        step_rows_amount = 10 ** 4
+        self.save_memory_evaluation_minidataset_to_csv(1)
+        while True:
+            test_eval_dataset_size = self.get_file_size_in_bytes(
+                'data/memory_evaluation_minidataset.csv'
+            )
+            print(f'Test dataset size = {test_eval_dataset_size} bytes')
+            self.rows_amount_for_minidataset += step_rows_amount
+            if test_eval_dataset_size < free_RAM_in_bytes:
+                self.save_memory_evaluation_minidataset_to_csv(self.rows_amount_for_minidataset)
+            else:
+                print('[INFO] Found rows amount of minidataset for your free RAM:')
+                print(f'nrows = {self.rows_amount_for_minidataset}')
+                return True
 
-    def get_approximately_rows_amount_for_minidataset(self):
-        free_memory_for_minidataset_in_bytes = self.free_memory_for_minidataset * 2 ** 30
-        rows_amount_for_minidataset = int(
-            free_memory_for_minidataset_in_bytes // self.memory_of_one_row_in_bytes
-        )
-        print(f'Rows amount for minidataset = {rows_amount_for_minidataset}')
-        return rows_amount_for_minidataset
-
-    def get_approximately_rows_amount_in_dataset(self):
-        dataset_size_in_bytes = self.get_file_size_in_bytes(self.csv_dataset_path)
-        approximately_rows_in_dataset = int(dataset_size_in_bytes / self.memory_of_one_row_in_bytes)
-        print(f'Approximately rows amount in dataset = {approximately_rows_in_dataset}')
-        return approximately_rows_in_dataset
-
-    def get_approximately_row_per_memory_in_bytes(self):
-        self.save_memory_evaluation_minidataset_to_csv()
-        memory_evaluation_minidataset_size_in_bytes = self.get_file_size_in_bytes(
-            'data/memory_evaluation_minidataset.csv')
-        print(f'Size of evaluation dataset = {memory_evaluation_minidataset_size_in_bytes} bytes')
-        approximately_memory_of_one_row_in_bytes = memory_evaluation_minidataset_size_in_bytes / 100
-        print(f'Approximately memory for one row = {approximately_memory_of_one_row_in_bytes}')
-        return approximately_memory_of_one_row_in_bytes
-
-    def save_memory_evaluation_minidataset_to_csv(self):
-        memory_evaluation_minidataset = pd.read_csv(self.csv_dataset_path, nrows = 100)
+    def save_memory_evaluation_minidataset_to_csv(self, rows_n = 1):
+        memory_evaluation_minidataset = pd.read_csv(self.csv_dataset_path, nrows = rows_n)
         memory_evaluation_minidataset.to_csv('data/memory_evaluation_minidataset.csv', index = False)
+        del memory_evaluation_minidataset
+        gc.collect()
 
     def get_file_size_in_bytes(self, file_path: str):
         return os.path.getsize(file_path)
 
 if __name__ == '__main__':
     dataset_splitter = Dataset_Splitting_by_free_RAM('data/user_reviews.csv')
-    dataset_splitter.main()
+    dataset_splitter.create_first_10_minidatasets_with_million_rows()
