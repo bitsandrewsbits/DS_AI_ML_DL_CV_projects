089380dc1ce982ebf812ac79044f4367312db0c7
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-02-04 21:43:03 +0200

    create method for cleaning data from missing values by columns or entire rows.

--- Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/minidatasets_preparing.py
+++ Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/minidatasets_preparing.py
@@ -11,9 +11,19 @@ class Minidatasets_Preparing:
 
     def prepare_one_minidataset(self, minidataset_number = 0):
         mini_df = pd.read_csv(self.datasets[minidataset_number])
-        print(mini_df.columns)
+        dataset_columns_names = mini_df.columns
+        self.clean_data_from_missing_values(mini_df)
+        print('Columns types:')
+        print(mini_df.dtypes)
+
+    def clean_data_from_missing_values(self, df):
+        df.dropna(axis = 1, how = 'any', inplace = True)
+        df.dropna(axis = 0, how = 'all', inplace = True)
+
+    def add_features_from_date_column(self):
+        pass
 
 if __name__ == "__main__":
     minidatasets = [f'data/minidataset_{i}' for i in range(1, 11)]
     data_preparing = Minidatasets_Preparing(minidatasets)
-    data_preparing.prepare_one_minidataset(2)
+    data_preparing.prepare_one_minidataset()
