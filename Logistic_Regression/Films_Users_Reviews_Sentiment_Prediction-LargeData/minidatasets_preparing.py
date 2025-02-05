8334b0e03d7716d302a8225b110040e6796d0e73
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-02-05 19:04:05 +0200

    create method for preparing date column and adding 3 features - year, month, day. create method for removing original date column from minidataset.

--- Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/minidatasets_preparing.py
+++ Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/minidatasets_preparing.py
@@ -5,23 +5,32 @@ import pandas as pd
 class Minidatasets_Preparing:
     def __init__(self, datasets: list):
         self.datasets = datasets
+        self.current_dataset = pd.DataFrame()
 
     def prepare_all_saved_minidatasets(self):
         pass
 
     def prepare_one_minidataset(self, minidataset_number = 0):
-        mini_df = pd.read_csv(self.datasets[minidataset_number])
-        dataset_columns_names = mini_df.columns
-        self.clean_data_from_missing_values(mini_df)
+        self.current_dataset = pd.read_csv(self.datasets[minidataset_number])
+        self.clean_data_from_missing_values()
+        self.add_features_from_date_column()
+        self.remove_original_date_column()
+        print(self.current_dataset)
         print('Columns types:')
-        print(mini_df.dtypes)
+        print(self.current_dataset.dtypes)
 
-    def clean_data_from_missing_values(self, df):
-        df.dropna(axis = 1, how = 'any', inplace = True)
-        df.dropna(axis = 0, how = 'all', inplace = True)
+    def clean_data_from_missing_values(self):
+        self.current_dataset.dropna(axis = 1, how = 'any', inplace = True)
+        self.current_dataset.dropna(axis = 0, how = 'all', inplace = True)
 
     def add_features_from_date_column(self):
-        pass
+        self.current_dataset['creationDate'] = pd.to_datetime(self.current_dataset['creationDate'])
+        self.current_dataset['creationYear'] = self.current_dataset['creationDate'].dt.year
+        self.current_dataset['creationMonth'] = self.current_dataset['creationDate'].dt.month
+        self.current_dataset['creationDay'] = self.current_dataset['creationDate'].dt.day
+
+    def remove_original_date_column(self):
+        self.current_dataset.drop('creationDate', axis = 1, inplace = True)
 
 if __name__ == "__main__":
     minidatasets = [f'data/minidataset_{i}' for i in range(1, 11)]
