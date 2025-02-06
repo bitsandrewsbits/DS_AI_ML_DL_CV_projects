98796d75034d8f27dd16da299b80758c17070df1
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-02-06 18:54:42 +0200

    create methods for getting unique movieIDs and stacking unique movieIDs from all minidatasets.

--- Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/minidatasets_preparing.py
+++ Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/minidatasets_preparing.py
@@ -1,11 +1,14 @@
 # Sentiment Analysis with created 10 Films Reviews minidatasets
 
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 class Minidatasets_Preparing:
     def __init__(self, datasets: list):
         self.datasets = datasets
         self.current_dataset = pd.DataFrame()
+        self.movieIDs_label_encoder = LabelEncoder()
+        self.all_unique_movieIDs = []
 
     def prepare_all_saved_minidatasets(self):
         pass
@@ -18,6 +21,7 @@ class Minidatasets_Preparing:
         print(self.current_dataset)
         print('Columns types:')
         print(self.current_dataset.dtypes)
+        self.add_only_new_unique_movieIDs()
 
     def clean_data_from_missing_values(self):
         self.current_dataset.dropna(axis = 1, how = 'any', inplace = True)
@@ -31,6 +35,24 @@ class Minidatasets_Preparing:
 
     def remove_original_date_column(self):
         self.current_dataset.drop('creationDate', axis = 1, inplace = True)
+
+    def encode_movieIDs_column(self):
+        for minidataset in self.datasets:
+            self.movieIDs_label_encoder.transform(minidataset)
+            # TODO: finish
+
+    def fit_all_classes_to_movieIDs_label_encoder(self):
+        self.movieIDs_label_encoder.fit(self.all_unique_movieIDs)
+
+    def add_only_new_unique_movieIDs(self):
+        current_unique_movieIDs = self.get_unique_movieIDs()
+        for unique_movieID in current_unique_movieIDs:
+            if unique_movieID not in self.all_unique_movieIDs:
+                self.all_unique_movieIDs.append(unique_movieID)
+        print('Total unique movieIDs =', len(self.all_unique_movieIDs))
+
+    def get_unique_movieIDs(self):
+        return pd.unique(self.current_dataset['movieId'])
 
 if __name__ == "__main__":
     minidatasets = [f'data/minidataset_{i}' for i in range(1, 11)]
