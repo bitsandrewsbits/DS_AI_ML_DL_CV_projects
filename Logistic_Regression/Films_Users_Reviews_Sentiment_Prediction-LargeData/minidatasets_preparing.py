172cfb659b2a4494c6ac7df448a8b80c8dfb8435
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-02-08 16:04:21 +0200

    create method for encoding boolean type columns. init label encoder for bool values.

--- Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/minidatasets_preparing.py
+++ Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/minidatasets_preparing.py
@@ -9,6 +9,7 @@ class Minidatasets_Preparing:
         self.current_dataset = pd.DataFrame()
         self.movieIDs_label_encoder = LabelEncoder()
         self.all_unique_movieIDs = []
+        self.boolean_label_encoder = LabelEncoder().fit([True, False])
 
     def prepare_all_saved_minidatasets(self):
         pass
@@ -18,10 +19,11 @@ class Minidatasets_Preparing:
         self.clean_data_from_missing_values()
         self.add_features_from_date_column()
         self.remove_original_date_column()
+        self.add_only_new_unique_movieIDs()
+        self.encode_boolean_type_columns()
         print(self.current_dataset)
         print('Columns types:')
         print(self.current_dataset.dtypes)
-        self.add_only_new_unique_movieIDs()
 
     def clean_data_from_missing_values(self):
         self.current_dataset.dropna(axis = 1, how = 'any', inplace = True)
@@ -38,8 +40,7 @@ class Minidatasets_Preparing:
 
     def encode_movieIDs_column(self):
         for minidataset in self.datasets:
-            self.movieIDs_label_encoder.transform(minidataset)
-            # TODO: finish
+            minidataset['movieId'] = self.movieIDs_label_encoder.transform(minidataset['movieId'])
 
     def fit_all_classes_to_movieIDs_label_encoder(self):
         self.movieIDs_label_encoder.fit(self.all_unique_movieIDs)
@@ -53,6 +54,14 @@ class Minidatasets_Preparing:
 
     def get_unique_movieIDs(self):
         return pd.unique(self.current_dataset['movieId'])
+
+    def encode_boolean_type_columns(self):
+        for column in self.current_dataset.columns:
+            if self.current_dataset[column].dtypes == bool:
+                self.current_dataset[column] = pd.Series(
+                    self.boolean_label_encoder.transform(
+                        self.current_dataset[column]
+                    ), dtype = 'int32')
 
 if __name__ == "__main__":
     minidatasets = [f'data/minidataset_{i}' for i in range(1, 11)]
