a13b925511100b89c2772ef7d71d91a0893b1de9
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-03-17 21:00:47 +0200

    create file with additional functions for all app files.

---
+++ Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/additional_functions.py
@@ -0,0 +1,11 @@
+# additional functions
+import re
+import os
+
+def get_minidatasets_filenames_in_data_dir():
+    minidatasets_filenames = []
+    data_dir_files = os.listdir('data')
+    for data_dir_file in data_dir_files:
+        if re.match(r'minidataset_[1-9][0-9]?.csv', data_dir_file):
+            minidatasets_filenames.append(data_dir_file)
+    return minidatasets_filenames
