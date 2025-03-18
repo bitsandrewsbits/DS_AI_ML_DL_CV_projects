8ace3276f9600633864831937506e4d9d67ad227
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-03-18 14:49:20 +0200

    add remove old result minidatasets function.

--- Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/additional_functions.py
+++ Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/additional_functions.py
@@ -9,3 +9,9 @@ def get_minidatasets_filenames_in_data_dir():
         if re.match(r'minidataset_[1-9][0-9]?.csv', data_dir_file):
             minidatasets_filenames.append(data_dir_file)
     return minidatasets_filenames
+
+def remove_old_result_minidatasets():
+    data_dir_files = os.listdir('data')
+    for file in data_dir_files:
+        if re.match(r'result_minidataset_.*', file):
+            os.remove(f'data/{file}')
