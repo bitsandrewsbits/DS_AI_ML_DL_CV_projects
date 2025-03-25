053a8784053fe48aa1fdf12ee2904b9b54a9e83f
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-03-25 21:00:41 +0200

    change processing steps for Text-to-Image model. continue testing COCO dataset API.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Text-to-Image_Generation/data_collection_and_preprocessing/test_cocoAPI.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Text-to-Image_Generation/data_collection_and_preprocessing/test_cocoAPI.py
@@ -1,17 +1,16 @@
 from pycocotools.coco import COCO
-from pycocotools import mask as maskUtils
 
 import numpy as np
 import skimage.io as io
 import matplotlib.pyplot as plt
 
-#data dirs - TRAIN data
+#data dirs - TEST data
 dataDir = 'data'
-dataType = 'train2017'
-annotationFile = f'{dataDir}/{dataType}/annotations/instances_{dataType}.json'
+dataType = 'test2017'
+annotation_file = f'{dataDir}/{dataType}/annotations/image_info_{dataType}.json'
 
 # initialize COCO API for instance annotations
-cocoAPI = COCO(annotationFile)
+cocoAPI = COCO(annotation_file)
 
 # display COCO categories and supercategories
 categories = cocoAPI.loadCats(cocoAPI.getCatIds())
@@ -19,3 +18,10 @@ categories_names = [category['name'] for category in categories]
 print(f"COCO categories: \n{categories_names}\n")
 supercategories = set([category['supercategory'] for category in categories])
 print(f'COCO supercategories:\n{supercategories}')
+
+test_categories = ['person', 'train']
+test_categories_IDs = cocoAPI.getCatIds(catNms = test_categories)
+print(test_categories_IDs)
+test_images_IDs = cocoAPI.getImgIds(catIds = test_categories_IDs)
+print('Test categories Images IDs:')
+print(test_images_IDs)
