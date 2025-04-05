e9db5f9fadc781a552b9a4b1beb948eb36f7ee85
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-04-05 15:38:16 +0300

    create new file - just separate script for executing datasets creations for Stable Diffusion model.

---
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Text-to-Image_Generation/data_collection_and_preprocessing/create_S_D_datasets.py
@@ -0,0 +1,25 @@
+# datasets creation for Stable Diffusion model(train, validation datasets)
+import dataset_creation_for_Stable_Diffusion_model as ds_for_sd
+
+datasets_info = {
+    "TRAIN": [
+        'train2017',
+        'instances_train2017.json',
+        'captions_train2017.json'],
+    "VALIDATION": [
+        'validation2017_for_fine-tune',
+        'instances_val2017.json',
+        'captions_val2017.json']
+}
+
+def create_datasets(datasets_info: dict):
+    for dataset_type in datasets_info:
+        ds_creation = ds_for_sd.Dataset_Creating_for_Stable_Diffusion(
+            dataset_info[dataset_type][0],
+            dataset_info[dataset_type][1],
+            dataset_info[dataset_type][2],
+        )
+        ds_creation.main(dataset_type)
+
+if __name__ == '__main__':
+    create_datasets(datasets_info)
