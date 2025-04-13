78bd948a83a1b58da1b3206160ef6ab138c60015
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-04-13 19:27:15 +0300

    update training script, but another error occured with CUDA memory.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Text-to-Image_Generation/stable_diffusion_model_fine-tune_training.sh
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Text-to-Image_Generation/stable_diffusion_model_fine-tune_training.sh
@@ -3,11 +3,10 @@ export HF_ENDPOINT=https://hf-mirror.com
 
 # Execute via accelerate:
 accelerate launch diffusers/examples/text_to_image/train_text_to_image.py \
+  --mixed_precision="fp16" \
   --pretrained_model_name_or_path="stable-diffusion-v1-5/stable-diffusion-v1-5" \
-  --image_column="image" \
-  --caption_column="image_annotations" \
   --snr_gamma=5.0 \
-  --dataset_name="data/validation2017_for_fine-tune/datasets_for_fine-tune" \
+  --dataset_name="data/validation2017_for_fine-tune/images" \
   --use_ema \
   --resolution=512 --center_crop --random_flip \
   --train_batch_size=1 \
@@ -16,5 +15,6 @@ accelerate launch diffusers/examples/text_to_image/train_text_to_image.py \
   --max_train_steps=15000 \
   --learning_rate=1e-05 \
   --max_grad_norm=1 \
+  --enable_xformers_memory_efficient_attention \
   --lr_scheduler="constant" --lr_warmup_steps=0 \
   --output_dir="trained_S_D_models"
