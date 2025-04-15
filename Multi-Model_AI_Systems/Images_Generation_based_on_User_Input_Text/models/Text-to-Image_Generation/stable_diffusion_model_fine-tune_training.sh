72863f8b7bb53630784866a9cf1e89da887d9f8c
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-04-15 21:14:30 +0300

    make changes in processing steps file, train script for train dataset.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Text-to-Image_Generation/stable_diffusion_model_fine-tune_training.sh
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Text-to-Image_Generation/stable_diffusion_model_fine-tune_training.sh
@@ -6,15 +6,15 @@ accelerate launch diffusers/examples/text_to_image/train_text_to_image.py \
   --mixed_precision="fp16" \
   --pretrained_model_name_or_path="stable-diffusion-v1-5/stable-diffusion-v1-5" \
   --snr_gamma=5.0 \
-  --dataset_name="data/validation2017_for_fine-tune/images" \
+  --dataset_name="data/train2017/images" \
   --use_ema \
   --resolution=512 --center_crop --random_flip \
   --train_batch_size=1 \
   --gradient_accumulation_steps=4 \
   --gradient_checkpointing \
-  --max_train_steps=15000 \
+  --max_train_steps=1000 \
   --learning_rate=1e-05 \
   --max_grad_norm=1 \
   --enable_xformers_memory_efficient_attention \
   --lr_scheduler="constant" --lr_warmup_steps=0 \
-  --output_dir="trained_S_D_models"
+  --output_dir="trained_S_D_model"
