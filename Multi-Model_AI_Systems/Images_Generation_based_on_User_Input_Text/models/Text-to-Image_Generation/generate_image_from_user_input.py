133bd79c4201e7cdeb1d9c70a42aa4f08deb22c1
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-04-17 22:31:48 +0300

    change generate image script.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Text-to-Image_Generation/generate_image_from_user_input.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Text-to-Image_Generation/generate_image_from_user_input.py
@@ -3,16 +3,18 @@ import torch
 
 trained_S_D_model_dir = "trained_S_D_model"
 
-def init_Stable_Diffusion_Pipeline():
-    S_D_pipeline = DiffusionPipeline.from_pretrained(
-    trained_S_D_model_dir, torch_dtype = torch.float16,  use_safetensors = True
+def get_Stable_Diffusion_Pipeline():
+    pipeline = DiffusionPipeline.from_pretrained(
+        trained_S_D_model_dir, torch_dtype = torch.float16,
+        use_safetensors = True
     ).to("cuda")
+    return pipeline
 
-def get_generated_PIL_image_obj(user_input: str):
-    prompt = "Dog on car."
-    PIL_image_obj = S_D_pipeline(prompt).images[0]
+def get_generated_PIL_image_obj(pipeline, user_input: str):
+    PIL_image_obj = pipeline(user_input).images[0]
     return PIL_image_obj
 
 if __name__ == '__main__':
-    init_Stable_Diffusion_Pipeline()
-    get_generated_PIL_image_obj()
+    S_D_pipeline = get_Stable_Diffusion_Pipeline()
+    user_input = "Cat reads a book."
+    result_image_obj = get_generated_PIL_image_obj(S_D_pipeline, user_input)
