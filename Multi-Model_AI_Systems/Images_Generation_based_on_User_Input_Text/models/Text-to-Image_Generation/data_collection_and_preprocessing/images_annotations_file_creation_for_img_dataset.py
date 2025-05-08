5b78401cf8c1fba14114a42a4338e01e81096d39
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-05-08 20:30:12 +0300

    make a change in imgs metadata dataset creation file - add restriction for validation dataset samples amount. and little other changes in other methods.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Text-to-Image_Generation/data_collection_and_preprocessing/images_annotations_file_creation_for_img_dataset.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Text-to-Image_Generation/data_collection_and_preprocessing/images_annotations_file_creation_for_img_dataset.py
@@ -3,6 +3,7 @@
 from pycocotools.coco import COCO
 import numpy as np
 import pandas as pd
+import re
 
 class Images_Annotations_File_Creation_For_Images_Dataset:
     def __init__(self, dataset_dir: str,
@@ -12,6 +13,8 @@ class Images_Annotations_File_Creation_For_Images_Dataset:
         self.images_dir = f'{self.dataDir}/{self.dataset_dir}/images'
         self.images_annotation_file = f'{self.dataDir}/{self.dataset_dir}/annotations/{images_annotation_file}'
         self.images_description_annot_file = f'{self.dataDir}/{self.dataset_dir}/annotations/{images_description_annot_file}'
+        self.validation_data_dir_regex = re.compile(r'.*validation.*')
+        self.validation_samples_amount = 500
 
         self.images_cocoAPI = COCO(self.images_annotation_file)
         self.annotations_cocoAPI = COCO(self.images_description_annot_file)
@@ -35,7 +38,12 @@ class Images_Annotations_File_Creation_For_Images_Dataset:
                     self.images_IDs_by_categories_IDs[category_ID]
                 )
             )
-        return unique_images_IDs
+        if re.match(self.validation_data_dir_regex, self.dataset_dir):
+            if len(unique_images_IDs) > self.validation_samples_amount:
+                return unique_images_IDs[:self.validation_samples_amount]
+            return unique_images_IDs
+        else:
+            return unique_images_IDs
 
     def main(self):
         self.define_images_amount_per_categories_IDs()
@@ -48,6 +56,7 @@ class Images_Annotations_File_Creation_For_Images_Dataset:
 
         self.convert_dataset_into_JSON_format()
         self.create_JSONL_images_metadata_file()
+        print(self.images_dataset_metadata)
 
     def define_images_amount_per_categories_IDs(self):
         for category_ID in self.images_IDs_by_categories_IDs:
@@ -79,7 +88,7 @@ class Images_Annotations_File_Creation_For_Images_Dataset:
     def add_image_filename_column_to_dataset(self):
         self.images_dataset_metadata["image"] = \
         self.images_dataset_metadata['image_ID'].apply(
-            self.get_image_filename
+            self.get_image_filename_path
         )
 
     def add_image_annotations_column_to_dataset(self):
@@ -88,9 +97,9 @@ class Images_Annotations_File_Creation_For_Images_Dataset:
             self.get_img_annotations
         )
 
-    def get_image_filename(self, img_id: int):
+    def get_image_filename_path(self, img_id: int):
         image_info = self.get_image_info_by_ID(img_id)
-        return image_info['file_name']
+        return f"{self.images_dir}/{image_info['file_name']}"
 
     def get_img_annotations(self, img_id: int):
         image_annotation_IDs = self.get_image_annotation_IDs(img_id)
