213ba81a63d61c53dc54264e9169cdbcab2c6f63
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-06-16 17:00:25 +0300

    create method for extracting review ID, rating from filename.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/BERT_fine-tuning_dataset_creation.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/BERT_fine-tuning_dataset_creation.py
@@ -1,9 +1,18 @@
 # preprocessing Films Reviews Dataset files for BERT fine-tuning dataset creation.
+import additional_functions_for_data_preprocessing as ad_fncs
+import re
 
 class BERT_Fune_Tuning_Dataset_Creation:
-    def __init__(self, dataset_vocabulary_file_path: str):
+    def __init__(self, dataset_vocabulary_file_path: str,
+    positive_reviews_dir_path: str, negative_reviews_dir_path: str):
         self.dataset_vocab_file_path = dataset_vocabulary_file_path
         self.vocab_word_via_index_dict = {}
+
+        self.positive_reviews_filenames = ad_fncs.get_filenames_from_dir(positive_reviews_dir_path)
+        self.negative_reviews_filenames = ad_fncs.get_filenames_from_dir(negative_reviews_dir_path)
+
+        self.positive_reviews_info = {'review_ID': [], 'rating': [], 'review_text': []}
+        self.negative_reviews_info = {'review_ID': [], 'rating': [], 'review_text': []}
 
     def main(self):
         self.create_dict_for_access_vocabulary_word_via_index()
@@ -14,8 +23,13 @@ class BERT_Fune_Tuning_Dataset_Creation:
                 self.vocab_word_via_index_dict[i] = word[:-1]
         return True
 
+    def get_review_ID_and_review_rating_from_filename(self, filename: str):
+        review_ID_and_rating = re.split('[_.]', filename)[:-1]
+        return {'review_ID': review_ID_and_rating[0], 'rating': review_ID_and_rating[1]}
+
 if __name__ == '__main__':
-    dataset_files_preprocessing = BERT_Fune_Tuning_Dataset_Creation(
-        'data/imdb.vocab'
+    fine_tune_dataset_creation = BERT_Fune_Tuning_Dataset_Creation(
+        'data/imdb.vocab', 'data/train/pos', 'data/train/neg'
     )
-    dataset_files_preprocessing.main()
+    fine_tune_dataset_creation.main()
+    print(fine_tune_dataset_creation.get_review_ID_and_review_rating_from_filename('1169_8.txt'))
