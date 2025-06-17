1ca4c34ea98c169573eb898a8da54cb20ed37aad
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-06-18 00:00:00 +0300

    create method for creating reviews dataset with diff reviews samples amount.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/DistilBERT_fune-tuning_dataset_creation.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/DistilBERT_fune-tuning_dataset_creation.py
@@ -1,6 +1,7 @@
 # preprocessing Films Reviews Dataset files for DistilBERT fine-tuning dataset creation.
 import additional_functions_for_data_preprocessing as ad_fncs
 import re
+import numpy as np
 
 class DistilBERT_Fune_Tuning_Dataset_Creation:
     def __init__(self, dataset_vocabulary_file_path: str,
@@ -13,17 +14,50 @@ class DistilBERT_Fune_Tuning_Dataset_Creation:
         self.positive_reviews_filenames = ad_fncs.get_filenames_from_dir(positive_reviews_dir_path)
         self.negative_reviews_filenames = ad_fncs.get_filenames_from_dir(negative_reviews_dir_path)
 
-        self.positive_reviews_info = {'rating': [], 'review_text': []}
-        self.negative_reviews_info = {'rating': [], 'review_text': []}
+        self.result_reviews_dataset = []
 
-    def main(self):
+    def main(self, samples_amount = 'all'):
         self.create_dict_for_access_vocabulary_word_via_index()
+        self.result_reviews_dataset = self.create_result_dataset(samples_amount)
+        return self.result_reviews_dataset
 
     def create_dict_for_access_vocabulary_word_via_index(self):
         with open(self.dataset_vocab_file_path, 'r') as dataset_vocab_file:
             for (i, word) in enumerate(dataset_vocab_file):
                 self.vocab_word_via_index_dict[i] = word[:-1]
         return True
+
+    def create_result_dataset(self, samples_amount) -> list[dict]:
+        positive_reviews_dataset = []
+        negative_reviews_dataset = []
+        positive_reviews_dataset += self.get_reviews_info_from_filenames(
+            self.positive_reviews_dir_path, self.positive_reviews_filenames
+        )
+        negative_reviews_dataset += self.get_reviews_info_from_filenames(
+            self.negative_reviews_dir_path, self.negative_reviews_filenames
+        )
+        if samples_amount == 'all':
+            return positive_reviews_dataset + negative_reviews_dataset
+        else:
+            positive_samples_amount = samples_amount // 2
+            negative_samples_amount = samples_amount - positive_samples_amount
+            result_pos_samples = list(np.random.choice(
+                positive_reviews_dataset, positive_samples_amount, replace = False
+            ))
+            result_neg_samples = list(np.random.choice(
+                negative_reviews_dataset, negative_samples_amount, replace = False
+            ))
+            return result_pos_samples + result_neg_samples
+
+    def get_reviews_info_from_filenames(self,
+    reviews_dir_path: str, reviews_filenames: list) -> list[dict]:
+        result_reviews_info = []
+        for review_filename in reviews_filenames:
+            review_rating = self.get_review_rating_from_filename(review_filename)
+            review_text = self.get_review_text_from_file(reviews_dir_path, review_filename)
+            review_info = {"rating": int(review_rating), "text": review_text}
+            result_reviews_info.append(review_info)
+        return result_reviews_info
 
     def get_review_rating_from_filename(self, filename: str):
         review_ID_and_rating = re.split('[_.]', filename)[:-1]
@@ -40,6 +74,5 @@ if __name__ == '__main__':
     fine_tune_dataset_creation = DistilBERT_Fune_Tuning_Dataset_Creation(
         'data/imdb.vocab', 'data/train/pos', 'data/train/neg'
     )
-    fine_tune_dataset_creation.main()
-    print('test rating:', fine_tune_dataset_creation.get_review_rating_from_filename('1169_8.txt'))
-    fine_tune_dataset_creation.get_review_text_from_file('data/train/pos', '1169_8.txt')
+    part_of_result_dataset = fine_tune_dataset_creation.main()
+    print(part_of_result_dataset)
