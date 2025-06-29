cb7e53c833bbf5027d1ebf536958af224d08e032
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-06-29 20:26:02 +0300

    add DistilBERT tokenizer, func for preprocess review text for mapping func.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/DistilBERT_FineTuning.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/DistilBERT_FineTuning.py
@@ -1,16 +1,21 @@
 # DistilBERT model fine-tuning
 import json
+from transformers import AutoTokenizer
 
 parent_dir = "data_collection_and_preprocessing"
 datasets_filename = "train_validation_test_datasets.json"
 
 def main():
     train_val_test_datasets = load_train_validation_test_datasets()
+    tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
 
 def load_train_validation_test_datasets():
     with open(f"{parent_dir}/{datasets_filename}", 'r') as datasets_f:
-        datasets = json.load(datasets_f)
-    return datasets
+        loaded_datasets = json.load(datasets_f)
+    return loaded_datasets
+
+def preprocess_review_text(review: dict):
+    return tokenizer(review["text"], truncation = True)
 
 if __name__ == '__main__':
     main()
