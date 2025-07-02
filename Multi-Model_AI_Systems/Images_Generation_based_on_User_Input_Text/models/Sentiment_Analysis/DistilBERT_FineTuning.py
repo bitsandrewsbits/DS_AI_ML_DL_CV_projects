c3d806182e55e6c01303e28238e50d4cf6c1cc4f
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-07-02 22:31:24 +0300

    continue to convert datasets in JSON format into dtype for tokenizer. not work yet.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/DistilBERT_FineTuning.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/DistilBERT_FineTuning.py
@@ -1,6 +1,7 @@
 # DistilBERT model fine-tuning
 import json
 from transformers import AutoTokenizer
+from datasets import DatasetDict
 
 parent_dir = "data_collection_and_preprocessing"
 datasets_filename = "train_validation_test_datasets.json"
@@ -8,14 +9,15 @@ datasets_filename = "train_validation_test_datasets.json"
 def main():
     train_val_test_datasets = load_train_validation_test_datasets()
     tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
+    # tokenized_datasets = train_val_test_datasets.map(
+    #     lambda dataset: tokenizer(dataset["text"], truncation = True),
+    #     batched = True
+    # )
 
-def load_train_validation_test_datasets():
+def load_train_validation_test_datasets() -> DatasetDict:
     with open(f"{parent_dir}/{datasets_filename}", 'r') as datasets_f:
         loaded_datasets = json.load(datasets_f)
-    return loaded_datasets
-
-def preprocess_review_text(review: dict):
-    return tokenizer(review["text"], truncation = True)
+    return DatasetDict(loaded_datasets)
 
 if __name__ == '__main__':
     main()
