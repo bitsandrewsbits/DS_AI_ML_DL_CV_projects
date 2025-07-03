861944be8a6096c1f2c5175e67aca4dbcd560602
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-07-03 21:04:33 +0300

    delete a datasets loading in JSON-style func. need to recreate.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/DistilBERT_FineTuning.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/DistilBERT_FineTuning.py
@@ -1,23 +1,21 @@
 # DistilBERT model fine-tuning
-import json
 from transformers import AutoTokenizer
-from datasets import DatasetDict
 
 parent_dir = "data_collection_and_preprocessing"
 datasets_filename = "train_validation_test_datasets.json"
 
 def main():
     train_val_test_datasets = load_train_validation_test_datasets()
+    print(train_val_test_datasets)
     tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
     # tokenized_datasets = train_val_test_datasets.map(
     #     lambda dataset: tokenizer(dataset["text"], truncation = True),
     #     batched = True
     # )
 
+# TODO: create func with respect to changed datasets data structure
 def load_train_validation_test_datasets() -> DatasetDict:
-    with open(f"{parent_dir}/{datasets_filename}", 'r') as datasets_f:
-        loaded_datasets = json.load(datasets_f)
-    return DatasetDict(loaded_datasets)
+    pass
 
 if __name__ == '__main__':
     main()
