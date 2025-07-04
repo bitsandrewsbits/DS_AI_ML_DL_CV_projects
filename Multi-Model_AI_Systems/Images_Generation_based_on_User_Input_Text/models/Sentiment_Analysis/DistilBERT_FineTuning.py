0f814de6ed295d2976fa8deb68ef06abf4882ee6
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-07-04 21:45:08 +0300

    create load-datasets function for load datasets in DatasetDict format. Uncomment tokenized_datasets creation for testing.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/DistilBERT_FineTuning.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/DistilBERT_FineTuning.py
@@ -1,21 +1,23 @@
 # DistilBERT model fine-tuning
 from transformers import AutoTokenizer
+from datasets import DatasetDict
 
-parent_dir = "data_collection_and_preprocessing"
-datasets_filename = "train_validation_test_datasets.json"
+datasets_parent_dir = "data_collection_and_preprocessing"
 
 def main():
-    train_val_test_datasets = load_train_validation_test_datasets()
+    train_val_test_datasets = load_train_val_test_datasets(datasets_parent_dir)
     print(train_val_test_datasets)
     tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
-    # tokenized_datasets = train_val_test_datasets.map(
-    #     lambda dataset: tokenizer(dataset["text"], truncation = True),
-    #     batched = True
-    # )
+    tokenized_datasets = train_val_test_datasets.map(
+        lambda dataset: tokenizer(dataset["text"], truncation = True),
+        batched = True
+    )
 
-# TODO: create func with respect to changed datasets data structure
-def load_train_validation_test_datasets() -> DatasetDict:
-    pass
+def load_train_val_test_datasets(datasets_parent_dir_path: str) -> DatasetDict:
+    loaded_datasets = DatasetDict.load_from_disk(
+        dataset_dict_path = datasets_parent_dir_path
+    )
+    return loaded_datasets
 
 if __name__ == '__main__':
     main()
