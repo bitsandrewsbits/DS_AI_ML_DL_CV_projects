c6db6fcaebdbae5db9d97878dd3ba8485d516cc8
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-07-05 20:08:18 +0300

    add data-collator. add compute-accuracy metric function.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/DistilBERT_FineTuning.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/DistilBERT_FineTuning.py
@@ -1,16 +1,22 @@
 # DistilBERT model fine-tuning
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, DataCollatorWithPadding
 from datasets import DatasetDict
+import evaluate
+import numpy
 
 datasets_parent_dir = "data_collection_and_preprocessing"
+accuracy = evaluate.load("accuracy")
 
 def main():
     train_val_test_datasets = load_train_val_test_datasets(datasets_parent_dir)
-    print(train_val_test_datasets)
     tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
     tokenized_datasets = train_val_test_datasets.map(
         lambda dataset: tokenizer(dataset["text"], truncation = True),
         batched = True
+    )
+    print(tokenized_datasets)
+    data_collator = DataCollatorWithPadding(
+        tokenizer = tokenizer, return_tensors = "tf"
     )
 
 def load_train_val_test_datasets(datasets_parent_dir_path: str) -> DatasetDict:
@@ -18,6 +24,11 @@ def load_train_val_test_datasets(datasets_parent_dir_path: str) -> DatasetDict:
         dataset_dict_path = datasets_parent_dir_path
     )
     return loaded_datasets
+
+def compute_accuracy_metric(evaluated_prediction):
+    predictions, labels = evaluated_prediction
+    predictions = np.argmax(predictions, axis = 1)
+    return accuracy.compute(predictions = predictions, references = labels)
 
 if __name__ == '__main__':
     main()
