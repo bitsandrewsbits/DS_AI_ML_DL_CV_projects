7e331c64f22cf31cb44cc52c6d3715adce31d6da
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-07-09 20:12:18 +0300

    create two func - getting converted to tf.data.Dataset type for Keras API

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/DistilBERT_FineTuning.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/DistilBERT_FineTuning.py
@@ -2,9 +2,10 @@
 from transformers import AutoTokenizer, DataCollatorWithPadding
 from transformers import TrainingArguments, TFAutoModelForSequenceClassification
 from transformers import create_optimizer
-from datasets import DatasetDict
+from datasets import DatasetDict, Dataset
 import evaluate
 import numpy as np
+import tensorflow as tf
 
 datasets_parent_dir = "data_collection_and_preprocessing"
 accuracy = evaluate.load("accuracy")
@@ -44,6 +45,11 @@ def main():
         init_learing_rate, warmup_steps, total_train_batch_steps
     )
 
+    train_val_test_tf_datasets = get_converted_all_datasets(
+        model, tokenized_datasets, batch_size, data_collator
+    )
+    print(train_val_test_tf_datasets)
+
 def load_train_val_test_datasets(datasets_parent_dir_path: str) -> DatasetDict:
     loaded_datasets = DatasetDict.load_from_disk(
         dataset_dict_path = datasets_parent_dir_path
@@ -59,6 +65,24 @@ def get_optimizer_and_schedule(learning_rate, warmup_steps, train_steps):
     return create_optimizer(
         init_lr = learning_rate, num_warmup_steps = warmup_steps,
         num_train_steps = train_steps
+    )
+
+def get_converted_all_datasets(
+model, datasets: DatasetDict, batch_size, collate_func) -> tf.data.Dataset:
+    tf_datasets = {}
+    for dataset_type in datasets:
+        tf_datasets[dataset_type] = get_converted_tf_dataset(
+            model, datasets[dataset_type], batch_size, collate_func
+        )
+    return tf_datasets
+
+def get_converted_tf_dataset(
+model, dataset: Dataset, batch_size, collate_func) -> tf.data.Dataset:
+    return model.prepare_tf_dataset(
+        dataset,
+        shuffle = True,
+        batch_size = batch_size,
+        collate_fn = collate_func
     )
 
 if __name__ == '__main__':
