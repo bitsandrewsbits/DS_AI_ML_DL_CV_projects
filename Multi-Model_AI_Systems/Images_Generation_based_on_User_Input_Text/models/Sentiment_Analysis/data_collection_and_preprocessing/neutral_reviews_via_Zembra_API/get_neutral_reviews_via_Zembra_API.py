23b10f3c56c9fb5a2e51fe0f682834d68be0ffe5
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-07-21 22:19:17 +0300

    create function for getting neutral reviews via Zembra ReviewsAPI in dataset form.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Zembra_API/get_neutral_reviews_via_Zembra_API.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Zembra_API/get_neutral_reviews_via_Zembra_API.py
@@ -2,6 +2,7 @@ import json
 import requests
 import openapi_client
 from openapi_client.rest import ApiException
+import pandas as pd
 
 sandbox_token = "your_zembra_sandbox_token"
 network_name = 'google'
@@ -12,20 +13,28 @@ configuration = openapi_client.Configuration(
   access_token = f"Bearer {sandbox_token}"
 )
 
-with openapi_client.ApiClient(configuration) as api_client:
-  api_instance = openapi_client.ReviewsApi(api_client)
-  network = openapi_client.ReviewNetwork(network_name)
-  try:
-    api_response = api_instance.get_review_job(
-      network = network, slug = slug, fields = ['text', 'rating'], 
-      has_replies = False, 
-      min_rating = 3, max_rating = 3, language = 'en',
-      include_deleted = True, limit = 2
-    )
-    reviews_objects = api_response.data.reviews
-    for review_obj in reviews_objects:
-      print('Review Text:')
-      print(review_obj.text)
-      print(f'Review Rating: {review_obj.rating}')
-  except ApiException as e:
-    print(e)
+reviews_max_amount = 2
+
+def get_neutral_reviews_via_ReviewsAPI(reviews_max_amount = 2) -> pd.DataFrame:
+  neutral_reviews = {"text": [], "label": []}
+  with openapi_client.ApiClient(configuration) as api_client:
+    api_instance = openapi_client.ReviewsApi(api_client)
+    network = openapi_client.ReviewNetwork(network_name)
+    try:
+      api_response = api_instance.get_review_job(
+        network = network, slug = slug, fields = ['text', 'rating'], 
+        has_replies = False, 
+        min_rating = 3, max_rating = 3, language = 'en',
+        include_deleted = True, limit = reviews_max_amount
+      )
+      reviews_objects = api_response.data.reviews
+      for review_obj in reviews_objects:
+        neutral_reviews["text"].append(review_obj.text)
+        neutral_reviews["label"].append(review_obj.rating)
+    except ApiException as e:
+      print(e)
+  print(pd.DataFrame(neutral_reviews))
+  return 0
+
+if __name__ == "__main__":
+  get_neutral_reviews_via_ReviewsAPI(reviews_max_amount)
