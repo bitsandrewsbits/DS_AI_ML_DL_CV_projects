f3e5fceea718ed721c28d89a69678819f7c2fb94
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-07-25 17:24:24 +0300

    add main func. add funcs for cleaning and filter some reviews as not very informative.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Zembra_API/get_neutral_reviews_via_Zembra_API.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Zembra_API/get_neutral_reviews_via_Zembra_API.py
@@ -3,6 +3,7 @@ import openapi_client
 from openapi_client.models.create_review_job_request import CreateReviewJobRequest
 from openapi_client.rest import ApiException
 import pandas as pd
+import numpy as np
 
 API_token = "your_API_token"
 network_name = "google"
@@ -13,9 +14,14 @@ configuration = openapi_client.Configuration(
   access_token = f"Bearer {API_token}"
 )
 
+def main():
+    neutral_reviews_df = get_neutral_reviews_via_ReviewsAPI()
+    filter_neutral_reviews_df(neutral_reviews_df)
+    save_neutral_reviews_as_JSON_file(neutral_reviews_df)
+
 def get_neutral_reviews_via_ReviewsAPI(max_reviews_from_API = 1000) -> pd.DataFrame:
-  neutral_reviews = {"text": [], "label": []}
-  with openapi_client.ApiClient(configuration) as api_client:
+    neutral_reviews = {"text": [], "label": []}
+    with openapi_client.ApiClient(configuration) as api_client:
     api_instance = openapi_client.ReviewsApi(api_client)
     network = openapi_client.ReviewNetwork(network_name)
     job_request_json = json.dumps({
@@ -26,35 +32,49 @@ def get_neutral_reviews_via_ReviewsAPI(max_reviews_from_API = 1000) -> pd.DataFr
       "max_rating": 3
     })
     # review_job_request = openapi_client.CreateReviewJobRequest.from_json(job_request_json)   # uncomment if start first time
-    job_id = "3a753e21-b383-408f-a485-92749e1db1d4"
-    uid = "8a8be640-16d6-40dc-885f-71c207b2e75f"
-    fields=["text", "rating"]
+    job_id = "a4fd659f-3b48-4e11-8db0-85f99869f3a4"
+    uid = "8a8be640-16d6-40dc-885f-71c207b2e75f"   # change from print(reviews_job)
+    fields = ["text", "rating"]
     try:
-      # reviews_job = api_instance.create_review_job(review_job_request)    # uncomment if start first time
-      # print(reviews_job)        # uncomment if start first time
-      api_response = api_instance.get_review_job(
-        network = network, 
-        slug = slug,
-        job_id = job_id, 
-        uid = uid, 
-        fields = fields,
-        min_rating = 3.0,
-        max_rating = 3.0,
-        limit = max_reviews_from_API,
-      )
-      reviews_objects = api_response.data.reviews
-      for review_obj in reviews_objects:
-        if review_obj.text != "" and review_obj.text != None:
-          neutral_reviews["text"].append(review_obj.text)
-          neutral_reviews["label"].append(review_obj.rating)
+        # reviews_job = api_instance.create_review_job(review_job_request)    # uncomment if start first time
+        # print(reviews_job)        # uncomment if start first time
+        api_response = api_instance.get_review_job(
+            network = network,
+            slug = slug,
+            job_id = job_id,
+            uid = uid,
+            fields = fields,
+            min_rating = 3.0,
+            max_rating = 3.0,
+            limit = max_reviews_from_API,
+        )
+        reviews_objects = api_response.data.reviews
+        for review_obj in reviews_objects:
+            if review_obj.text != "" and review_obj.text != None:
+                neutral_reviews["text"].append(review_obj.text)
+                neutral_reviews["label"].append(review_obj.rating)
     except ApiException as e:
-      print(e)
-  print(pd.DataFrame(neutral_reviews))
-  return pd.DataFrame(neutral_reviews)
+        print(e)
+    return pd.DataFrame(neutral_reviews)
+
+def filter_and_clean_neutral_reviews_df(reviews_df: pd.DataFrame):
+    reviews_df["text"] = reviews_df["text"].apply(
+        get_review_text
+    )
+    reviews_df.dropna(axis = 0, inplace = True, ignore_index = True)
+
+def get_review_text(text):
+    text_words = text.split(' ')
+    if len(text_words) > 2:
+        return text
+    else:
+        return np.nan
 
 def save_neutral_reviews_as_JSON_file(dataframe: pd.DataFrame):
-  dataframe.to_json("neutral_reviews.json", orient = 'records', lines = True)
+    dataframe.to_json("neutral_reviews.json", orient = 'records', lines = True)
 
 if __name__ == "__main__":
-  neutral_reviews_df = get_neutral_reviews_via_ReviewsAPI()
-  save_neutral_reviews_as_JSON_file(neutral_reviews_df)
+    main()
+    neutral_reviews_df = pd.read_json(
+        "neutral_reviews.json", orient = 'records', lines = True
+    )
