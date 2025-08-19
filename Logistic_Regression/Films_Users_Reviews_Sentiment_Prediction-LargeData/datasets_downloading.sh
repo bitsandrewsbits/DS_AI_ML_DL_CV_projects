5a640043be2d37f556402eb2cd23ca2acc3c47c9
Andrew Kovalenko <kov.andreas.30@gmail.com>
2025-08-19 22:52:56 +0300

    add mkdir cmd, unzip cmd for exctructing datasets from archive file.

--- Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/datasets_downloading.sh
+++ Logistic_Regression/Films_Users_Reviews_Sentiment_Prediction-LargeData/datasets_downloading.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+#1 create dir for datasets
+mkdir data
+
+#2 download datasets archive
 curl -L -o data/movie-critic-and-user-reviews.zip\
   https://www.kaggle.com/api/v1/datasets/download/bwandowando/rotten-tomatoes-9800-movie-critic-and-user-reviews
+
+#3 unzip archive to data dir
+unzip -j data/movie-critic-and-user-reviews.zip -d data
