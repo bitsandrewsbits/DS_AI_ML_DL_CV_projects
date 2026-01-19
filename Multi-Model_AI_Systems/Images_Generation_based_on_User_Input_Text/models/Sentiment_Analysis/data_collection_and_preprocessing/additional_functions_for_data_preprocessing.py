5ffe7990e5cfe56bf46f96348ec5625de9a56c4f
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-01-19 22:21:52 +0200

    add create-dir function to add-functions file.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/additional_functions_for_data_preprocessing.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/additional_functions_for_data_preprocessing.py
@@ -4,6 +4,9 @@ import os
 def get_filenames_from_dir(dir_relative_path: str):
     return os.listdir(dir_relative_path)
 
+def create_directory(dirname: str):
+    os.mkdir(dirname)
+
 if __name__ == '__main__':
     filenames = get_filenames_from_dir('data/train/pos')
     print(filenames)
