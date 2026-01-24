224ee1a5c997c5c061fb07412d9b711544cf8a8a
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-01-24 19:28:03 +0200

    change code structure in order to use correctly in other scripts.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/download_datasets.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/download_datasets.py
@@ -2,30 +2,34 @@ import os
 
 downloaded_datasets_root_dir = "downloaded_datasets"
 dataset = {
-    "dataset_name": "Large_Movie_Review_Dataset",
+    "dataset_name": "Large_Movie_Reviews_Dataset",
     "URL": "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 }
 
-if downloaded_datasets_root_dir in os.listdir('.'):
-    print('[INFO] Root datasets dir already exist.')
-else:
-    print("[INFO] Root datasets dir doesn't exist on disk. Creating...")
-    os.system(f"mkdir {downloaded_datasets_root_dir}")
+def download_large_movie_reviews_dataset(dataset_info: dict):
+    if downloaded_datasets_root_dir in os.listdir('.'):
+        print('[INFO] Root datasets dir already exist.')
+    else:
+        print("[INFO] Root datasets dir doesn't exist on disk. Creating...")
+        os.system(f"mkdir {downloaded_datasets_root_dir}")
 
-if dataset["dataset_name"] in os.listdir(f'{downloaded_datasets_root_dir}'):
-    print(f"[INFO] Dataset '{dataset["dataset_name"]}' already downloaded.")
-else:
-    print(f"[INFO] Dataset dir '{dataset["dataset_name"]}' doesn't exist on disk.")
-    print(f"[INFO] Creating dataset dir '{dataset["dataset_name"]}'...")
-    os.system(f"mkdir {downloaded_datasets_root_dir}/{dataset["dataset_name"]}")
-    print(f"[INFO] Downloading into {dataset["dataset_name"]}'...")
-    # download Large Movie Review Dataset as archive:
-    os.system(f"curl {dataset["URL"]} --output {downloaded_datasets_root_dir}/\{dataset['dataset_name']}/large_movie_review_dataset.tar.gz")
-    # unzip dataset:
-    print("[INFO] Unzipping and preparing for further py-scripts...")
-    os.system(f"gzip -d {downloaded_datasets_root_dir}/{dataset['dataset_name']}/large_movie_review_dataset.tar.gz")
-    os.system(f"tar -xf {downloaded_datasets_root_dir}/{dataset['dataset_name']}/large_movie_review_dataset.tar")
-    # move files from aclImdb dir -> {dataset["dataset_name"]} dir:
-    os.system(f"mv aclImdb/* {downloaded_datasets_root_dir}/{dataset["dataset_name"]}")
-    # remove empty aclImdb dir:
-    os.system(f"rm -r aclImdb")
+    if dataset["dataset_name"] in os.listdir(f'{downloaded_datasets_root_dir}'):
+        print(f"[INFO] Dataset '{dataset_info["dataset_name"]}' already downloaded.")
+    else:
+        print(f"[INFO] Dataset dir '{dataset_info["dataset_name"]}' doesn't exist on disk.")
+        print(f"[INFO] Creating dataset dir '{dataset_info["dataset_name"]}'...")
+        os.system(f"mkdir {downloaded_datasets_root_dir}/{dataset_info["dataset_name"]}")
+        print(f"[INFO] Downloading into {dataset_info["dataset_name"]}'...")
+        # download Large Movie Review Dataset as archive:
+        os.system(f"curl {dataset_info["URL"]} --output {downloaded_datasets_root_dir}/{dataset_info['dataset_name']}/large_movie_review_dataset.tar.gz")
+        # unzip dataset:
+        print("[INFO] Unzipping and preparing for further py-scripts...")
+        os.system(f"gzip -d {downloaded_datasets_root_dir}/{dataset_info['dataset_name']}/large_movie_review_dataset.tar.gz")
+        os.system(f"tar -xf {downloaded_datasets_root_dir}/{dataset_info['dataset_name']}/large_movie_review_dataset.tar")
+        # move files from aclImdb dir -> {dataset["dataset_name"]} dir:
+        os.system(f"mv aclImdb/* {downloaded_datasets_root_dir}/{dataset_info["dataset_name"]}")
+        # remove empty aclImdb dir:
+        os.system(f"rm -r aclImdb")
+
+if __name__ == "__main__":
+    download_large_movie_reviews_dataset(dataset)
