6f526443b6167978cca5c55d0b8ed2204769e739
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-02-05 23:52:41 +0200

    change functions for cleaning reviews texts via pandas apply method. update spec chars list.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/unlabeled_reviews_cleaning.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/unlabeled_reviews_cleaning.py
@@ -11,7 +11,6 @@ from nltk.corpus import stopwords
 unlabeled_reviews_dir_path = f"../{load_ds.downloaded_datasets_root_dir}/{load_ds.dataset['dataset_name']}/train"
 unlabeled_dataset_creator = ordds.One_Reviews_Dir_Dataset_Creator(unlabeled_reviews_dir_path, 'unsup')
 unlabeled_dataset = unlabeled_dataset_creator.main()
-print(unlabeled_dataset)
 
 def main(dataset: pd.DataFrame) -> pd.DataFrame:
     dataset = convert_review_column_to_lowercase(dataset)
@@ -20,27 +19,39 @@ def main(dataset: pd.DataFrame) -> pd.DataFrame:
     return cleaned_dataset
 
 def convert_review_column_to_lowercase(dataset: pd.DataFrame):
+    print("[INFO] Converting reviews text to lowercase...")
     text_column = dataset['text']
     dataset['text'] = text_column.str.lower()
     return dataset
 
 def remove_any_spec_chars_in_review_column(dataset: pd.DataFrame):
-    spec_chars = [',', '.', '!', '?', ':', ';', '/', '@', '#']
-    for i in range(len(dataset['text'])):
-        for spec_char in spec_chars:
-            spec_char_regex = rf"\{spec_char}"
-            target_review_text = dataset['text'][i]
-            dataset.loc[i, 'text'] = re.sub(spec_char_regex, '', target_review_text)
+    print("[INFO] Removing special characters from reviews...")
+    spec_chars = [
+        ',', '.', '!', '?', ':', ';', '/', '@', '#', '\'', '(', ')', '<', '>',
+        '{', '}', '*', '\\', '~', '&', '-', '$', '=', '|'
+    ]
+    reviews_column_for_updating = dataset['text']
+    for spec_char in spec_chars:
+        spec_char_regex = rf"\{spec_char}"
+        reviews_column_for_updating = reviews_column_for_updating.apply(
+            lambda review_text: re.sub(spec_char_regex, '', review_text)
+        )
+    dataset['text'] = reviews_column_for_updating
     return dataset
 
 def remove_stop_words_in_review_column(dataset: pd.DataFrame):
+    print("[INFO] Removing stop words from reviews...")
     en_stopwords = stopwords.words(fileids = 'english')
-    for i in range(len(dataset['text'])):
-        for stopword in en_stopwords:
-            stopword_regex = rf"^{stopword} | {stopword} "
-            target_review_text = dataset['text'][i]
-            dataset.loc[i, 'text'] = re.sub(stopword_regex, ' ', target_review_text)
+    reviews_column_for_updating = dataset['text']
+    for stopword in en_stopwords:
+        stopword_regex = rf"^{stopword} | {stopword} "
+        reviews_column_for_updating = reviews_column_for_updating.apply(
+            lambda review_text: re.sub(stopword_regex, ' ', review_text)
+        )
+    dataset['text'] = reviews_column_for_updating
     return dataset
+
+# TODO: create func for saving cleaned dataset in JSON
 
 if __name__ == "__main__":
     cleaned_dataset = main(unlabeled_dataset)
