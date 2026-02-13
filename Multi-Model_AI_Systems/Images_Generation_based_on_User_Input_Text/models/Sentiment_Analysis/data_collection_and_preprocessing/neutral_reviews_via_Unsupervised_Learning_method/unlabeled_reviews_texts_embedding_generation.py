7fc54c80274479f5f3e95de35807518788034fbc
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-02-13 23:56:08 +0200

    create functions wrapping into class and methods.Class constructions around one object - dataset JSONL file.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/unlabeled_reviews_texts_embedding_generation.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/unlabeled_reviews_texts_embedding_generation.py
@@ -4,14 +4,34 @@ import pandas as pd
 
 embed_model_name = "embeddinggemma"
 ollama_host = "localhost"
+ollama_port = "11434"
 unlabeled_reviews_dataset_file = "cleaned_unlabeled_reviews_dataset.jsonl"
 
-def main(dataset_filename: str):
-    unlabeled_dataset = get_dataframe_from_JSONL_file(dataset_filename)
-    print(unlabeled_dataset)
+class Texts_Embedding_Dataset_Generator:
+    def __init__(self, embed_model_name: str, ollama_host: str, ollama_port: str, dataset_file: str):
+        self.embed_model_name = embed_model_name
+        self.ollama_host = ollama_host
+        self.ollama_port = ollama_port
+        self.ollama_client = self.get_Ollama_client()
+        self.dataset_JSONL_file = dataset_file
 
-def get_dataframe_from_JSONL_file(filename: str):
-    return pd.read_json(filename, orient = "records", lines = True)
+    def main(self):
+        unlabeled_dataset = self.get_dataframe_from_JSONL_file()
+        print(unlabeled_dataset)
+        self.load_embed_model_to_Ollama()
+
+    def get_dataframe_from_JSONL_file(self):
+        return pd.read_json(self.dataset_JSONL_file, orient = "records", lines = True)
+
+    def get_Ollama_client(self):
+        return olm.Client(f"http://{self.ollama_host}:{self.ollama_port}")
+    
+    def load_embed_model_to_Ollama(self):
+        print(f"[INFO] Loading {self.embed_model_name} model for embedding vectors generating...")
+        olm.pull(self.embed_model_name)
 
 if __name__ == "__main__":
-    main(unlabeled_reviews_dataset_file)
+    text_embed_generator = Texts_Embedding_Dataset_Generator(
+        embed_model_name, ollama_host, ollama_port, unlabeled_reviews_dataset_file
+    )
+    text_embed_generator.main()
