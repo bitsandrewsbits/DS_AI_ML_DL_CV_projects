1c1d53438865a7034765311e7789c69776e498da
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-02-23 22:41:17 +0200

    add method for defining approx execution time for embedding generation for entire dataset. add necessary class attributes and messages.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/unlabeled_reviews_texts_embedding_generation.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/unlabeled_reviews_texts_embedding_generation.py
@@ -10,15 +10,20 @@ unlabeled_reviews_dataset_file = "cleaned_unlabeled_reviews_dataset.jsonl"
 
 class Texts_Embedding_Dataset_Generator:
     def __init__(self, embed_model_name: str, ollama_host: str, ollama_port: str, dataset_file: str, computing_time_estimation_mode = False):
+        self.computing_time_estimation_mode = computing_time_estimation_mode
         self.start_execution_time = time.time()
-        self.execution_time = 0
+        self.execution_time_10_samples = 0
+        self.execution_time_for_entire_dataset = 0
+        
         self.embed_model_name = embed_model_name
         self.ollama_host = ollama_host
         self.ollama_port = ollama_port
         self.ollama_client = self.get_Ollama_client()
+        
         self.dataset_JSONL_file = dataset_file
         self.unlabeled_dataset = self.get_dataframe_from_JSONL_file()
-        if computing_time_estimation_mode:
+        
+        if self.computing_time_estimation_mode:
             print("[WARN] Embedding-computing-time-estimation is Enabled! Testing on 10 samples...")
             self.unlabeled_embedding_dataset = self.unlabeled_dataset.copy().iloc[:10]
         else:
@@ -26,11 +31,19 @@ class Texts_Embedding_Dataset_Generator:
 
     def main(self):
         self.load_embed_model_to_Ollama()
-        self.add_to_dataset_embedding_column()
-        print(self.unlabeled_embedding_dataset)
-        self.save_text_embedding_dataset_to_JSON()
-        self.define_execution_time()
-        print(f"[INFO] Execution time = {self.execution_time} seconds")
+        if self.computing_time_estimation_mode:
+            self.add_to_dataset_embedding_column()
+            print(self.unlabeled_embedding_dataset)
+            self.save_text_embedding_dataset_to_JSON()
+            self.define_execution_time_for_10_samples()
+            print(f"[INFO] Execution time (10 samples) = {self.execution_time_10_samples} seconds")
+            self.define_approx_execution_time_for_entire_dataset()
+            print("[INFO] Approximated execution time for entire dataset = ", end = '') 
+            print(f"{self.execution_time_for_entire_dataset} seconds or ", end = '')
+            print(f"{self.execution_time_for_entire_dataset / 60} minutes")
+        else:
+            self.add_to_dataset_embedding_column()
+            self.save_text_embedding_dataset_to_JSON()
 
     def get_dataframe_from_JSONL_file(self):
         return pd.read_json(self.dataset_JSONL_file, orient = "records", lines = True)
@@ -52,7 +65,7 @@ class Texts_Embedding_Dataset_Generator:
 
     # embed compute time for 50*10^3 reviews too much(approx 1 hour!)
     # TODO: think, how to setup ollama servers into cluster and compute in parallel embed vectors.
-    # TODO: think, maybe it will be convenient to generate a compose.yaml with beforehand Ollama containers amount. 
+    # TODO: think, maybe it will be convenient to generate a compose.yaml with beforehand Ollama containers amount.
     def add_to_dataset_embedding_column(self):
         print("[INFO] Generating text embedding vectors and adding to dataset...")
         self.unlabeled_embedding_dataset["embedding_vector"] = self.unlabeled_embedding_dataset["text"].apply(
@@ -69,9 +82,15 @@ class Texts_Embedding_Dataset_Generator:
             orient = "records"
         )
     
-    def define_execution_time(self):
+    def define_execution_time_for_10_samples(self):
         end_execution_time = time.time()
-        self.execution_time = end_execution_time - self.start_execution_time
+        self.execution_time_10_samples = round(end_execution_time - self.start_execution_time, 2)
+
+    def define_approx_execution_time_for_entire_dataset(self):
+        samples_amount = self.unlabeled_dataset.shape[0]
+        self.execution_time_for_entire_dataset = round(
+            (samples_amount / 10) * self.execution_time_10_samples, 2
+        )
 
 if __name__ == "__main__":
     text_embed_generator = Texts_Embedding_Dataset_Generator(
