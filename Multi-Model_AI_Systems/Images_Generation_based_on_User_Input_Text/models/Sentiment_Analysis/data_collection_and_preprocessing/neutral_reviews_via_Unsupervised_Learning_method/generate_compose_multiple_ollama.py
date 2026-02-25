ac8c7bcee41d96e417357a7d4ca32176d52381a5
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-02-25 20:49:49 +0200

    add few methods for ollama cluster compose config and generating compose.yaml file

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/generate_compose_multiple_ollama.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/generate_compose_multiple_ollama.py
@@ -1,5 +1,6 @@
 # script for generating compose.yaml for starting multiple Ollama container with diff ports
 from unlabeled_reviews_texts_embedding_generation import *
+import yaml
 
 class Compose_YAML_File_Generator:
     def __init__(self, embed_dataset_generator: object):
@@ -7,6 +8,14 @@ class Compose_YAML_File_Generator:
         self.embed_dataset_generation_time_in_sec = self.get_embed_dataset_generation_time()
         self.desired_embed_generation_time_in_sec = 300
         self.ollama_instances_for_parallel_computing = self.get_ollama_instances_for_parallel_computing()
+        self.first_ollama_service_port = 11434
+        self.compose_config = {"services": {}}
+        self.compose_yaml_strings = ""
+
+    def main(self):
+        self.define_ollama_cluster_compose_config()
+        self.define_compose_yaml_strings()
+        self.write_yaml_strings_to_compose_file()
 
     def get_embed_dataset_generation_time(self):
         self.embed_dataset_generator.main()
@@ -18,6 +27,23 @@ class Compose_YAML_File_Generator:
         )
         print(f"[INFO] Approximate Ollama server instances for parallel embed computing = {ollama_instances}")
         return ollama_instances
+    
+    def define_ollama_cluster_compose_config(self):
+        print("[INFO] Generating ollama cluster compose config...")
+        for i in range(self.ollama_instances_for_parallel_computing):
+            current_ollama_port = self.first_ollama_service_port + i
+            self.compose_config["services"][f"ollama-embed-{i}"] = {
+                "image": "ollama/ollama",
+                "ports": [f"{current_ollama_port}:11434"]
+            }
+    
+    def define_compose_yaml_strings(self):
+        self.compose_yaml_strings = yaml.dump(self.compose_config)
+    
+    def write_yaml_strings_to_compose_file(self):
+        print("[INFO] Saving YAML strings to compose.yaml...")
+        with open("ollama_cluster_compose.yaml", "w") as cmp_f:
+            cmp_f.write(self.compose_yaml_strings)
 
 if __name__ == "__main__":
     text_embed_generator = Texts_Embedding_Dataset_Generator(
@@ -26,3 +52,4 @@ if __name__ == "__main__":
         computing_time_estimation_mode = True
     )
     compose_generator = Compose_YAML_File_Generator(text_embed_generator)
+    compose_generator.main()
