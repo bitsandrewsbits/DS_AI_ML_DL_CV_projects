1c4e39365bd5ae0dac617951e80eadd1cba63c6a
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-03-02 21:56:16 +0200

    add methods for generating names for small datasets, reading JSONL file as pandas DF object. creating datasets dir if it doesn't exist

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/dataset_splitter.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/dataset_splitter.py
@@ -2,17 +2,37 @@
 # small amount define by disired embed generation compute time per ollama container.
 import generate_compose_multiple_ollama as gcmo
 import unlabeled_reviews_texts_embedding_generation as urteg
+import pandas as pd
+import os
 
 class Dataset_Splitter:
-    def __init__(self, dataset_file: str):
-        self.dataset_file = dataset_file
+    def __init__(self, dataset_JSONL_file: str):
+        self.dataset_file = dataset_JSONL_file
+        self.dataset = self.get_dataframe_from_JSONL_file()
         self.text_embed_generator = self.get_text_embed_generator()
         self.compose_generator = gcmo.Compose_YAML_File_Generator(self.text_embed_generator)
         self.target_datasets_amount = self.compose_generator.ollama_instances_for_parallel_computing
-        self.datasets_filenames = []
+        self.datasets_filenames = self.get_datasets_names()
+        self.datasets_dir = "datasets_for_parallel_embed_generation"
     
     def main(self):
+        if os.path.exists(self.datasets_dir):
+            print("[INFO] Datasets dir for parallel embed generation exists!")
+        else:
+            print("[INFO] Creating datasets dir for parallel embed generation...")
+            os.mkdir(self.datasets_dir)
+    
+    def split_dataset_into_small_sets(self):
+        # TODO: create method
         pass
+
+    def get_datasets_names(self):
+        return [
+            f"dataset_#{ds_number}" for ds_number in range(1, self.target_datasets_amount + 1)
+        ]
+    
+    def get_dataframe_from_JSONL_file(self):
+        return pd.read_json(self.dataset_file, orient = "records", lines = True)
 
     def get_text_embed_generator(self):
         return urteg.Texts_Embedding_Dataset_Generator(
@@ -22,5 +42,5 @@ class Dataset_Splitter:
         )
 
 if __name__ == "__main__":
-        self.compose_yaml_strings = ""
     ds_splitter = Dataset_Splitter("cleaned_unlabeled_reviews_dataset.jsonl")
+    ds_splitter.main()
