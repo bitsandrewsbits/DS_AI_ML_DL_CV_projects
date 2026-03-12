6452c164919c7e249e7e7c3239f0379b06209bc1
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-03-12 15:30:57 +0200

    update bash script in order to create/start containerwithout displaying error with one of two commands.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/start_ollama_for_compute_time_estimation.sh
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/start_ollama_for_compute_time_estimation.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo docker run --name embed_gen_time_estimation -p 11434:11434 -d ollama/ollama
+container_name="embed_gen_time_estimation"
 
+sudo docker start $container_name 2> /dev/null
+sudo docker run --name $container_name -p 11434:11434 -d ollama/ollama 2> /dev/null
