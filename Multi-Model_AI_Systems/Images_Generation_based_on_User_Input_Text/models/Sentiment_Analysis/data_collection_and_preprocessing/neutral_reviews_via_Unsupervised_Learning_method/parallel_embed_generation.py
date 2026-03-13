5a6f07684b8ff2161ab66b3f5fd8b0222519df36
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-03-13 22:15:42 +0200

    remove load/save dataset method. add embed_dataset_generator_obj-creator method for parallel generation readiness

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/parallel_embed_generation.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/parallel_embed_generation.py
@@ -3,6 +3,7 @@
 # -> concatenate to one dataset with embeddings.
 import os
 import pandas as pd
+import unlabeled_reviews_texts_embedding_generation as urteg
 
 small_sets_dirname = "datasets_for_parallel_embed_generation"
 
@@ -10,12 +11,18 @@ class Parallel_Embedding_Generation_Manager:
     def __init__(self, datasets_dir: str):
         self.datasets_dir = datasets_dir
         self.datasets_files_pathes = []
-        self.datasets_as_dataframes = []
+        self.ollama_services_amount = 0
+        self.ollama_host = "localhost"
+        self.first_ollama_service_port = 11434
+        self.embed_model = urteg.embed_model_name
+        self.embed_dataset_generator_objects = []
 
     def main(self):
         if os.path.exists(self.datasets_dir):
             self.define_datasets_files_pathes()
-            self.load_datasets()
+            self.define_ollama_services_amount()
+            self.create_embed_generator_objects()
+            print(self.embed_dataset_generator_objects)
         else:
             print("Dir doesn't exist!")
 
@@ -25,17 +32,26 @@ class Parallel_Embedding_Generation_Manager:
             self.datasets_files_pathes.append(
                 f"{self.datasets_dir}/{set_file}"
             )
+    
+    def define_ollama_services_amount(self):
+        self.ollama_services_amount = len(self.datasets_files_pathes)
 
-    def load_datasets(self):
-        print("[INFO] Loading small datasets...", end = '')
-        for dataset_file_path in self.datasets_files_pathes:
-            self.datasets_as_dataframes.append(
-                self.get_dataframe_from_JSONL_file(dataset_file_path)
+    def create_embed_generator_objects(self):
+        print("[INFO] Create embed dataset generator objects...")
+        for (i, dataset_file) in enumerate(self.datasets_files_pathes):
+            current_ollama_port = self.first_ollama_service_port + i
+            self.embed_dataset_generator_objects.append(
+                self.get_review_embed_generator_obj(
+                    self.embed_model, self.ollama_host,
+                    current_ollama_port, dataset_file
+                )
             )
-        print(f"loaded {len(self.datasets_as_dataframes)} sets.")
-
-    def get_dataframe_from_JSONL_file(self, dataset_file: str):
-        return pd.read_json(dataset_file, orient = "records", lines = True)
+    
+    def get_review_embed_generator_obj(self, embed_model, ollama_host, ollama_port, dataset_file):
+        return urteg.Texts_Embedding_Dataset_Generator(
+            embed_model, ollama_host, ollama_port,
+            dataset_file
+        )
 
 if __name__ == "__main__":
     parallel_embed_gen_manager = Parallel_Embedding_Generation_Manager(small_sets_dirname)
