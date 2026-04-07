bf95cce9ac5a8e8f1348e4315a13234a4aa09341
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-04-07 21:55:40 +0300

    update create-dir func, add save-dataset-to-JSONL func

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/additional_functions_for_data_preprocessing.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/additional_functions_for_data_preprocessing.py
@@ -1,11 +1,21 @@
 # additional functions
 import os
+import pandas as pd
 
 def get_filenames_from_dir(dir_relative_path: str):
     return os.listdir(dir_relative_path)
 
-def create_directory(dirname: str):
-    os.mkdir(dirname)
+def create_directory(dir_path: str):
+    if dir_path in os.listdir('.'):
+        print(f"[INFO] Dir {dir_path} already exist!")
+    else:
+        print(f"[INFO] Creating {dir_path} dir.")
+        os.mkdir(dir_path)
+
+def save_dataset_into_JSONL(df: pd.DataFrame, dataset_path: str):
+    df.to_json(
+        dataset_path, orient = 'records', lines = True
+    )
 
 if __name__ == '__main__':
     filenames = get_filenames_from_dir('data/train/pos')
