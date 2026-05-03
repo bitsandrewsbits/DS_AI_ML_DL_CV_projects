8116316b9730a5607ef7dbaa8fd75c80e0801130
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-05-03 21:58:07 +0300

    add few functions for images pxs normalization and get-labels-amount

--- Computer_Vision/CNN_Projects/CIFAR-10_Classification/NN_for_images_classification_via_tensorflow.py
+++ Computer_Vision/CNN_Projects/CIFAR-10_Classification/NN_for_images_classification_via_tensorflow.py
@@ -1,9 +1,23 @@
 import tensorflow as tf
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.layers import Flatten, Input, Dense, Conv2D
+import numpy as np
 
 def main():
     (train_X, train_y), (test_X, test_y) = cifar10.load_data()
+    train_X = normalize_images_px_values(train_X)
+    test_X = normalize_images_px_values(test_X)
+    images_classes_amount = get_images_classes_amount(train_y)
+
+def get_CNN_model(classes_amount: int):
+    # TODO: think, how to create min model for image classification
+    pass
+
+def normalize_images_px_values(dataset_X: np.array):
+    return dataset_X / 255
+
+def get_images_classes_amount(dataset_Y: np.array):
+    return len(np.unique(dataset_Y))
 
 if __name__ == '__main__':
     main()
