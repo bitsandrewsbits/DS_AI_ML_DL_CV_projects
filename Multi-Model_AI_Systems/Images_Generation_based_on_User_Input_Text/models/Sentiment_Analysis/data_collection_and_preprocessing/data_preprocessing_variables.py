9fd7738780e733605a0b6935e399418850947131
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-05-14 22:34:13 +0300

    add few global vars to global-vars file.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/data_preprocessing_variables.py
+++ Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/data_preprocessing_variables.py
@@ -1,12 +1,18 @@
 # variables for data preprocessing scripts
 DOWNLOADED_DATASETS_ROOT_DIR = "downloaded_datasets"
 PREPROCESSED_ORIGINAL_DATASETS_DIR = "preprocessed_original_reviews_datasets"
+USERS_SENTIMENTS_DATASET_INFO = {
+    "dataset_name": "Large_Movie_Reviews_Dataset",
+    "URL": "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+}
 
 MODEL_FOR_EMBEDDING_GENERATION = "mrutkows/granite-embedding:30m"
 TEMP_CONTAINER_OLLAMA_HOST = "localhost"
 TEMP_CONTAINER_OLLAMA_PORT = "11434"
+EMBED_COMPUTE_TIME_ESTIMATION_SET_NAME = "set_for_embed_compute_time_estimation.jsonl"
 CLEANED_UNLABELED_REVIEWS_DATASET = "cleaned_unlabeled_reviews_dataset.jsonl"
 
+SMALL_SETS_FOR_EMBED_GENERATION_DIR = "datasets_for_parallel_embed_generation"
 REVIEWS_EMBEDDING_DATASETS_DIR = "review_datasets_with_embeddings"
 BIG_REVIEWS_EMBED_DATASET = "big_reviews_embedding_dataset.jsonl"
 
