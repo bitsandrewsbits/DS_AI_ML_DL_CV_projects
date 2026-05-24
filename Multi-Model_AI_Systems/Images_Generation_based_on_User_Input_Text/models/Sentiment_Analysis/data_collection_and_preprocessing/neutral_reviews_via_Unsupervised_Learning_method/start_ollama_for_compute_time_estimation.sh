25077899b5d0b1c71c4799677c977a197139d2f1
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-05-24 20:43:18 +0300

    delete bash script for manually run ollama container for embed gen compute time estimation.

--- Multi-Model_AI_Systems/Images_Generation_based_on_User_Input_Text/models/Sentiment_Analysis/data_collection_and_preprocessing/neutral_reviews_via_Unsupervised_Learning_method/start_ollama_for_compute_time_estimation.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-container_name="embed_gen_time_estimation"
-
-sudo docker start $container_name 2> /dev/null
-sudo docker run --name $container_name -p 11434:11434 -d ollama/ollama 2> /dev/null
