fc261ef016b7ee1dda67ad412dc1a82204c3ca97
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-06-25 23:18:10 +0300

    add two global vars for license plate YOLO model

--- Computer_Vision/Automobile_Traffic_Analyzer/global_vars.py
+++ Computer_Vision/Automobile_Traffic_Analyzer/global_vars.py
@@ -1,2 +1,5 @@
 AUTOMOBILE_DETECTION_MODEL_NAME = "yolov8s-world.pt"
-DETECTION_CLASSES = ["car"]
+AUROMOBILE_DETECTION_CLASSES = ["car"]
+
+LICENSE_PLATE_DETECTION_MODEL_URL = "https://huggingface.co/morsetechlab/yolov11-license-plate-detection/resolve/main/license-plate-finetune-v1l.pt?download=true"
+LICENSE_PLATE_DETECTION_MODEL_NAME = "license-plate-finetune-v1l.pt"
