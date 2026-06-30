b9cc72718c8717cd27011c56d7f8172c269597d4
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-06-30 17:43:04 +0300

    add glob vars for session detected cars file var

--- Computer_Vision/Automobile_Traffic_Analyzer/global_vars.py
+++ Computer_Vision/Automobile_Traffic_Analyzer/global_vars.py
@@ -6,3 +6,5 @@ AUROMOBILE_DETECTION_CLASSES = ["car"]
 
 LICENSE_PLATE_DETECTION_MODEL_URL = "https://huggingface.co/morsetechlab/yolov11-license-plate-detection/resolve/main/license-plate-finetune-v1l.pt?download=true"
 LICENSE_PLATE_DETECTION_MODEL_NAME = "license-plate-finetune-v1l.pt"
+
+SESSION_DETECTED_CARS_FILENAME = "session_detected_cars.jsonl"
