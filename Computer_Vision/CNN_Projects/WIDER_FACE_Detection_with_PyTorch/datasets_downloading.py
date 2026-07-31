c2dd6c137f15e1b2ab8f04c2f307fa89317234c1
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-07-31 22:35:53 +0300

    add file for downloading WIDER FACE datasets via URLs.

---
+++ Computer_Vision/CNN_Projects/WIDER_FACE_Detection_with_PyTorch/datasets_downloading.py
@@ -0,0 +1,43 @@
+# downloading WIDER FACE train/val/test datasets
+import requests
+import zipfile
+import os
+from pathlib import Path
+
+def main(datasets_info: dict):
+	datasets_path = create_datasets_dirs()
+	for dataset_type, url in datasets_info.items():
+		if f"WIDER_{dataset_type}" in os.listdir(datasets_path):
+			print(f"Dataset WIDER_{dataset_type} already downloaded! skipping.")
+		else:
+			download_dataset_zip(datasets_path, dataset_type, url)
+			unzip_dataset(datasets_path, dataset_type)
+
+def create_datasets_dirs() -> Path:
+	root_path = Path("data")
+	datasets_path = root_path / "WIDER_sets"
+	if root_path.is_dir():
+		print("Datasets dir already exist!")
+	else:
+		print("Creating datasets dir...")
+		datasets_path.mkdir(parents = True, exist_ok = True)
+	return datasets_path
+
+def download_dataset_zip(datasets_path: Path, dataset_type: str, url: str):
+	print(f"Downloading {dataset_type} WIDER dataset...")
+	with open(datasets_path / f"WIDER_{dataset_type}.zip", "wb") as f:
+		response = requests.get(url)
+		f.write(response.content)
+
+def unzip_dataset(datasets_path: Path, dataset_type: str):
+	print("Extracting data...")
+	with zipfile.ZipFile(datasets_path / f"WIDER_{dataset_type}.zip", "r") as r_f:
+		r_f.extractall(datasets_path)
+
+if __name__ == "__main__":
+	datasets_URLs = {
+		"train": "https://huggingface.co/datasets/CUHK-CSE/wider_face/resolve/main/data/WIDER_train.zip",
+		"validation": "https://huggingface.co/datasets/CUHK-CSE/wider_face/resolve/main/data/WIDER_val.zip",
+		"test": "https://huggingface.co/datasets/CUHK-CSE/wider_face/resolve/main/data/WIDER_test.zip"
+	}
+	main(datasets_URLs)
