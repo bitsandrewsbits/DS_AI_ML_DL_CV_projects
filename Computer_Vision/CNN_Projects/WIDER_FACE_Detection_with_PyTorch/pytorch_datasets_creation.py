3acf800877574afa7f8cb43bbd19c447b40450ef
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-08-05 20:24:18 +0300

    add TODO. change method for get-image-pathes.

--- Computer_Vision/CNN_Projects/WIDER_FACE_Detection_with_PyTorch/pytorch_datasets_creation.py
+++ Computer_Vision/CNN_Projects/WIDER_FACE_Detection_with_PyTorch/pytorch_datasets_creation.py
@@ -1,29 +1,31 @@
 # data preparation, PyTorch-datasets creation
 import os
-import pathlib
+from pathlib import Path
 
 class Datasets_Praparation:
 	def __init__(self):
-		self.data_root_path = pathlib.Path("data/WIDER_sets")
+		self.data_root_path = Path("data/WIDER_sets")
 		self.data_pathes = {
-			"train": self.data_root_path / "WIDER_train",
-			"validation": self.data_root_path / "WIDER_val",
-			"test": self.data_root_path / "WIDER_test"
+			"train": self.data_root_path / "WIDER_train" / "images",
+			"val": self.data_root_path / "WIDER_val" / "images",
+			"test": self.data_root_path / "WIDER_test" / "images",
 		}
+		self.data_annotations_root_path = self.data_root_path / "wider_face_split"
+		
+		# TODO: create custom dataset class from 
+		# DatasetCustom output format - image_tensor_transformed, (bbx1(x1, y1, h, w), bbx2(x2, y2, h, w), ... bbxn(xm, yn, h, w))
 
 	def main(self):
 		for dataset_path in self.data_pathes.values():
-			self.move_images_to_datasets_root_dir(dataset_path)
+			self.get_dataset_images_abs_pathes(dataset_path)
 
-	def move_images_to_datasets_root_dir(self, root_data_path: pathlib.Path):
-		for path_obj in root_data_path.walk():
-			temp_dir_objects = os.listdir(path_obj[0])
-			for dir_obj in temp_dir_objects:
-				dir_obj_path = path_obj[0] / dir_obj
+	def get_dataset_images_abs_pathes(self, root_data_path: Path):
+		paths = list(root_data_path.glob("*/*.jpg"))
+		return paths
 
 	# TODO: think, how to parce wider_face annotation.txt file with
 	# bounding boxes parameters and add to image filename as target labels.
-	def get_image_path(self)
+	def get_image_path(self):
 		# TODO: get image path via regex
 		pass
 
