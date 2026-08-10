8eca8e21d2774b3d9ec1c6f5c98f300e32c61c65
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-08-10 21:32:55 +0300

    add image transform struct. add to main pytorch-dataset-creation for each dataset type.

--- Computer_Vision/CNN_Projects/WIDER_FACE_Detection_with_PyTorch/pytorch_datasets_creation.py
+++ Computer_Vision/CNN_Projects/WIDER_FACE_Detection_with_PyTorch/pytorch_datasets_creation.py
@@ -1,6 +1,10 @@
 # data preparation, PyTorch-datasets creation
 import os
 from pathlib import Path
+import torch
+from torchvision.transforms import v2
+
+from face_detection_dataset import Wider_Face_Detection_Dataset
 
 class Datasets_Praparation:
 	def __init__(self):
@@ -12,11 +16,29 @@ class Datasets_Praparation:
 		}
 		self.data_annotation_paths = {
 			"train": self.data_root_path / "wider_face_split" / "wider_face_train_bbx_gt.txt",
-			"val": self.data_root_path / "wider_face_split" / "wider_face_val_bbx_gt.txt"
+			"val": self.data_root_path / "wider_face_split" / "wider_face_val_bbx_gt.txt",
+			"test": None
+		}
+		self.image_transforms = v2.Compose([
+			v2.ToImage(),
+			v2.Resize(size = (224, 224)),
+			v2.RandomHorizontalFlip(p = 0.5),
+			v2.ToDtype(torch.float32, scale = True)
+		])
+		self.pytorch_datasets = {
+			"train": '',
+			"val": '',
+			"test": ''
 		}
 
 	def main(self):
-		pass
+		for dataset_type, data_path in self.data_pathes.items():
+			self.pytorch_datasets[dataset_type] = Wider_Face_Detection_Dataset(
+				data_path, self.data_annotation_paths[dataset_type],
+				self.image_transforms
+			)
+		# TODO: think, how to create dataloaders for each dataset.
+		# TODO: create dataloaders to access them from external side(from classifier file)
 
 if __name__ == "__main__":
 	datasets_prep = Datasets_Praparation()
