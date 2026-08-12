7d1ab24577145fc80aeed5c2284f8a4ff6a56f99
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-08-12 21:55:11 +0300

    add collate function for correct batch struct with changable label size.

--- Computer_Vision/CNN_Projects/WIDER_FACE_Detection_with_PyTorch/pytorch_datasets_creation.py
+++ Computer_Vision/CNN_Projects/WIDER_FACE_Detection_with_PyTorch/pytorch_datasets_creation.py
@@ -3,12 +3,13 @@ import os
 from pathlib import Path
 import torch
 from torchvision.transforms import v2
+from torchvision.tv_tensors import BoundingBoxes
 from torch.utils.data import DataLoader
 
 from face_detection_dataset import Wider_Face_Detection_Dataset
 
 class Datasets_Praparation:
-	def __init__(self):
+	def __init__(self, image_size = (64, 64), batch_size = 32):
 		self.data_root_path = Path("data/WIDER_sets")
 		self.data_pathes = {
 			"train": self.data_root_path / "WIDER_train" / "images",
@@ -22,7 +23,7 @@ class Datasets_Praparation:
 		}
 		self.image_transforms = v2.Compose([
 			v2.ToImage(),
-			v2.Resize(size = (224, 224)),
+			v2.Resize(size = image_size),
 			v2.RandomHorizontalFlip(p = 0.5),
 			v2.ToDtype(torch.float32, scale = True)
 		])
@@ -31,7 +32,7 @@ class Datasets_Praparation:
 			"val": '',
 			"test": ''
 		}
-		self.batch_size = 2
+		self.batch_size = batch_size
 		self.dataloaders = {
 			"train": '',
 			"val": '',
@@ -45,7 +46,6 @@ class Datasets_Praparation:
 				self.image_transforms
 			)
 		train_sample = self.pytorch_datasets["train"][0]
-		print("train sample shape:", train_sample[0].shape, train_sample[1])
 		for dataset_type in self.dataloaders.keys():
 			if dataset_type != "test":
 				self.dataloaders[dataset_type] = DataLoader(
@@ -59,20 +59,25 @@ class Datasets_Praparation:
 					dataset = self.pytorch_datasets[dataset_type],
 					batch_size = self.batch_size
 				)
-		sample_train_batch = next(iter(self.dataloaders["train"]))
-		print(len(sample_train_batch))
-		print(sample_train_batch[0].shape)
-		print("Target len:", len(sample_train_batch[1]))
-		print("Target shape:", sample_train_batch[1][0].shape)
 
-		# TODO: think, how to create dataloaders for each dataset.
-		# TODO: create dataloaders to access them from external side(from classifier file)
-
-	def collate_func(self, batch: tuple[torch.Tensor, list]):
+	def collate_func(self, batch: tuple[torch.Tensor, BoundingBoxes]):
 		image_batch = []
 		label_batch = []
-		# TODO: create collate_fn()
+		for image, bbxs_obj in batch:
+			image_batch.append(image)
+			label_batch.append(bbxs_obj)
+		return image_batch, label_batch
+
+	def inspect_train_dataloader_struct(self):
+		sample_train_batch = next(iter(self.dataloaders["train"]))
+		print("Batch amount in train-loader:", len(self.dataloaders["train"]))
+		print(sample_train_batch[0])
+		print(sample_train_batch[1])
 
 if __name__ == "__main__":
-	datasets_prep = Datasets_Praparation()
+	datasets_prep = Datasets_Praparation(
+		image_size = (224, 224),
+		batch_size = 2
+	)
 	datasets_prep.main()
+	datasets_prep.inspect_train_dataloader_struct()
