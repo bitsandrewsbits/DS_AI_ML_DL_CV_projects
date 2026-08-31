5a9b56cedb0ff4d12fa501d0b611c87ae6ee47f1
Andrew Kovalenko <kov.andreas.30@gmail.com>
2026-08-31 23:01:08 +0300

    add new file - additional funcs after model training process.

---
+++ Computer_Vision/CNN_Projects/WIDER_FACE_Detection_with_PyTorch/utils.py
@@ -0,0 +1,32 @@
+# additional functions after training models
+import matplotlib.pyplot as plt
+from pathlib import Path
+import os
+
+def show_loss_curve_plot(epochs: list, losses: list, loss_type: str):
+	plt.plot(epochs, losses, label = f'{loss_type}-loss')
+	plt.xlabel('epoch')
+	plt.ylabel('loss')
+	plt.legend()
+	plt.grid(True)
+
+def create_trained_model_dir(trained_models_root_path: Path, cv_task: str):
+	trained_models_for_cv_task_root_path = trained_models_root_path / cv_task
+	trained_models_for_cv_task_root_path.mkdir(parents = True, exist_ok = True)
+	
+	trained_model_dirname_pattern = "training_#_"
+	trained_models_cv_task = os.listdir(trained_models_for_cv_task_root_path)
+	
+	if trained_models_cv_task != 0:
+		current_training_num = len(trained_models_cv_task) + 1
+		trained_model_dirname = f"{trained_model_dirname_pattern}{current_training_num}"
+	else:
+		trained_model_dirname = f"{trained_model_dirname_pattern}0";
+	
+	trained_model_dir_path = trained_models_for_cv_task_root_path / trained_model_dirname
+	trained_model_dir_path.mkdir(parents = True, exist_ok = True)
+
+def save_loss_curves_plot(self, plot_name: str):
+	pass
+		
+self.trained_model_dir_path = self.get_trained_model_dir_path()
